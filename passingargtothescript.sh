18411ad4e58ab552bf1b99c619423b7df11cb1a2
marou9916 <oumar.ndiaye447@gmail.com>
2023-06-15 22:27:15 +0200

    Learning to pass arguments to a script

---
+++ passingargtothescript.sh
@@ -0,0 +1,16 @@
+#!usr/bin/bash
+function File {
+    # think you are inside the file
+    # Change here
+    echo $#
+}
+
+# Do not change anything
+if [ ! $# -lt 1 ]; then
+    File $*
+    exit 0
+fi
+
+# change here
+# here you can pass the arguments
+bash passingargtothescript.sh Shell is fun
